535d7db70097b1c78c06764c7f742f843c735c0a
YATENDRA AWANA <yka6739@gmail.com>
2021-08-19 14:40:57 +0530

    Add project files.

---
+++ WebApplication18/Client/Pages/Myclass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication18.Client.Pages
+{
+    public class Myclass
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+}
